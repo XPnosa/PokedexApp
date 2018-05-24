654d475fb16021e7fc687317fe5a04c4c5097d23
XPnosa <gm.mizuki@gmail.com>
2018-05-24 17:01:22 +0200

    FIXES

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -4,6 +4,10 @@ var pokedex = {}
 
 var generation = 0;
 
+var last_pokemon = 807;
+
+var load_completed = false;
+
 var app = {
 	initialize: function() {
 		this.bindEvents();
@@ -12,6 +16,8 @@ var app = {
 		document.addEventListener('deviceready', this.onDeviceReady, false);
 	},
 	onDeviceReady: function() {
+		dontBack();
+		document.addEventListener("backbutton", onBackKeyDown, false);
 		app.receivedEvent('deviceready');
 	},
 	receivedEvent: function(id) {
@@ -25,6 +31,7 @@ function readJson(filePath) {
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 			document.getElementById("list").innerHTML = ''; fitDex()
+			var fix = document.getElementById("legend").innerHTML
 			var pokemon = JSON.parse(xmlhttp.responseText);
 			var len = Object.keys(pokemon).length;
 			for (i=1;i<=len;i++) {
@@ -35,14 +42,18 @@ function readJson(filePath) {
 			var refreshIntervalId = setInterval(function() {
 				var pkmn = i<10?"00"+i:i<100?"0"+i:i
 				if ( i <= ( len ) ) {
-					printPokedex(pkmn);
+					var percent = (i/last_pokemon*100).toFixed(0)+"%"
+					document.getElementById("legend").innerHTML = fix + 
+					'<div class="init inactive"><div class="init">&nbsp;' + percent + '&nbsp;</div></div>'
+					setTimeout(function(){
+						printPokedex(pkmn);
+					}, 0);
 					if ( i == len ) {
 						showDex(0);
 						swipePkdex();
+						load_completed = true;
 					}
-				} else {
-					clearInterval(refreshIntervalId);
-				}
+				} else clearInterval(refreshIntervalId);
 				i++
 			}, 0);
 		}
@@ -53,11 +64,12 @@ function readJson(filePath) {
 function fitDex() {
 	var h1 = document.getElementById("body").offsetHeight-50;
 	document.getElementById("pkdex").style.height = h1+"px";
-	var h2 = document.getElementById("pkdex").offsetHeight-45;
+	if ( document.getElementById("legend").offsetHeight > 50 ) var h2 = document.getElementById("pkdex").offsetHeight-60;
+	else var h2 = document.getElementById("pkdex").offsetHeight-45;
 	document.getElementById("list").style.height = h2+"px";
 	var h3 = document.getElementById("body").offsetWidth-50;
 	document.getElementById("info").style.width = h3+"px";
-	var h4 = document.getElementById("info").offsetHeight-45;
+	var h4 = document.getElementById("info").offsetHeight-50;
 	document.getElementById("photo").style.height = h4+"px";
 	var photo = document.getElementById("photo-full")
 	if ( document.getElementById("body").offsetHeight < document.getElementById("body").offsetWidth ) {
@@ -80,6 +92,8 @@ function printPokedex(pkmn) {
 	var pokemon = "<div id='"+pkmn+"' class='pkmn "+gen+"'>";
 	pokemon += "<img class='pk_img' onclick='viewImage(\""+pkmn+"\")' src='pkmn/"+pkmn+".png' />"
 	pokemon += "<input type='button' onclick='showDetails(\""+pkmn+"\")' class='pk_name' value='"+pokedex[pkmn].nombre+"' />";
+	pokemon += "<input type='button' onclick='showDetails(\""+pkmn+"\")' class='pk_name altura' value='Altura: "+pokedex[pkmn].altura+"' />";
+	pokemon += "<input type='button' onclick='showDetails(\""+pkmn+"\")' class='pk_name peso' value='Peso: "+pokedex[pkmn].peso+"' />";
 	pokemon += "<div class='pk_types'>";
 	for (var t=0;t<pokedex[pkmn].tipo.length;t++) pokemon += "<div onclick='showDetails(\""+pkmn+"\")' class='pk_type "+pokedex[pkmn].tipo[t]+"'><span>"+pokedex[pkmn].tipo[t]+"</span></div>";
 	pokemon += "</div></div>";
@@ -89,25 +103,30 @@ function printPokedex(pkmn) {
 }
 
 function showDex(n) {
-	generation = n;
-	var list = document.querySelectorAll('.pkmn');
-	for(i=0; i<list.length; i++) list[i].style.display = "none";
-	document.getElementById("html").style.background = getBackground(n);
-	if (n==0) var legend = '<div onclick="shearchDex(\'Nacional\');" class="active"><div>Nacional</div></div>';
-	else var legend = '<div title="Pokedex Nacional" onclick="showDex(0);" class="inactive"><div>#</div></div>';
-	for (i=1;i<=7;i++) {
-		if (i==n) legend += '<div onclick="shearchDex(\''+getRegion(i)+'\');" class="G'+i+' active"><div>'+getRegion(i)+'</div></div>';
-		else legend += '<div title="Pokedex '+getRegion(i)+'" onclick="showDex('+i+');" class="G'+i+' inactive"><div>'+i+'</div></div>';
-	}
-	document.getElementById("legend").innerHTML = legend;
-	if (n==0) list = document.querySelectorAll('.pkmn');
-	else list = document.querySelectorAll('.'+getRegion(n));
-	for(i=0; i<list.length; i++) list[i].style.display = "";
-	document.getElementById("loading-mini").style.display='none';
+	document.getElementById("loading-mini").style.display='';
+	setTimeout(function(){
+		generation = n;
+		var list = document.querySelectorAll('.pkmn');
+		for(i=0; i<list.length; i++) list[i].style.display = "none";
+		document.getElementById("html").style.background = getBackground(n);
+		if (n==0) var legend = '<div onclick="shearchDex(\'Nacional\');" class="active"><div>Nacional</div></div>';
+		else var legend = '<div title="Pokedex Nacional" onclick="showDex(0);" class="inactive"><div>#</div></div>';
+		for (i=1;i<=7;i++) {
+			if (i==n) legend += '<div onclick="shearchDex(\''+getRegion(i)+'\');" class="G'+i+' active"><div>'+getRegion(i)+'</div></div>';
+			else legend += '<div title="Pokedex '+getRegion(i)+'" onclick="showDex('+i+');" class="G'+i+' inactive"><div>'+i+'</div></div>';
+		}
+		document.getElementById("legend").innerHTML = legend;
+		if (n==0) list = document.querySelectorAll('.pkmn');
+		else list = document.querySelectorAll('.'+getRegion(n));
+		for(i=0; i<list.length; i++) list[i].style.display = "";
+		document.getElementById("loading-mini").style.display='none';
+	}, 0);
+	
 }
 
 function shearchDex(region) {
 	var search = prompt("Buscar pokemon en la Pokedex " + region);
+	if ( search == null ) search = ""
 	if ( region == "Nacional" )
 		var list = document.querySelectorAll('.pkmn');
 	else
@@ -136,7 +155,8 @@ function showDetails(pkmn) {
 	var type = "<center><div class='pk_types_info'>";
 	for (var t=0;t<pokedex[pkmn].tipo.length;t++) type += "<div class='pk_type "+pokedex[pkmn].tipo[t]+"'><span>"+pokedex[pkmn].tipo[t]+"</span></div>";
 	type += "<div></center>";
-	info.innerHTML = "<p class='title'>" + pkmn + " - " + pokedex[pkmn].nombre + "</p>" + desc  + "<hr />" + type
+	info.innerHTML = "<img style='float: right;' class='pk_img' onclick='viewImage(\""+pkmn+"\")' src='pkmn/"+pkmn+".png' />"
+	info.innerHTML += "<p class='title'>" + pkmn + " - " + pokedex[pkmn].nombre + "</p>" + desc  + "<hr />" + type
 	details.style.display = ""; fitDex();
 }
 
@@ -248,6 +268,24 @@ function swipePkdex(){
 			if((touch.pageX<xIni-20) && (touch.pageY> yIni-5) && (touch.pageY<yIni+5) && (generation<7)) showDex(++generation);
 		}
 	}, false); 
+}
+
+function dontBack(){
+	window.location.hash="no-back-button";
+	window.location.hash="Again-No-back-button"
+	window.onhashchange=function(){
+		window.location.hash="no-back-button";
+		if (load_completed) showDex(generation);
+		closeImage();
+		closeDetails(); 
+	}
+}
+
+function onBackKeyDown(e) {
+	e.preventDefault();
+	if (load_completed) showDex(generation);
+	closeImage();
+	closeDetails(); 
 }
 
 $(window).bind('resize',function(e) { fitDex(); });
